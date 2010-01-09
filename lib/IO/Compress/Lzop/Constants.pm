6b8bd9197abf8d0ae5178540a237ab2d266f9a87
pmqs <pmqs@cpan.org>
2010-01-09 17:57:24 +0000

    import IO-Compress-Lzop 2.024
    git-cpan-module:   IO-Compress-Lzop
    git-cpan-version:  2.024
    git-cpan-authorid: pmqs

--- lib/IO/Compress/Lzop/Constants.pm
+++ lib/IO/Compress/Lzop/Constants.pm
@@ -8,7 +8,7 @@ require Exporter;
 
 our ($VERSION, @ISA, @EXPORT);
 
-$VERSION = '2.023';
+$VERSION = '2.024';
 
 @ISA = qw(Exporter);
 
--- lib/IO/Uncompress/Adapter/LZO.pm
+++ lib/IO/Uncompress/Adapter/LZO.pm
@@ -4,11 +4,11 @@ use strict;
 use warnings;
 use bytes;
 
-use IO::Compress::Base::Common  2.023 qw(:Status);
+use IO::Compress::Base::Common  2.024 qw(:Status);
 use Compress::LZO ;
 
 our ($VERSION, @ISA);
-$VERSION = '2.023';
+$VERSION = '2.024';
 
 
 sub mkUncompObject

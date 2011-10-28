8d3519e2e80ac3422488c8fa492a7f99dc74bd05
pmqs <pmqs@cpan.org>
2011-10-28 22:20:54 +0100

    import IO-Compress-Lzop 2.040
      2.040 28 October 2011
    
          * No Changes
    
    
    git-cpan-module:   IO-Compress-Lzop
    git-cpan-version:  2.040
    git-cpan-authorid: pmqs

--- lib/IO/Uncompress/UnLzop.pm
+++ lib/IO/Uncompress/UnLzop.pm
@@ -4,18 +4,18 @@ use strict ;
 use warnings;
 use bytes;
 
-use IO::Compress::Base::Common  2.039 qw(:Status createSelfTiedObject);
+use IO::Compress::Base::Common  2.040 qw(:Status createSelfTiedObject);
 
-use IO::Uncompress::Base  2.039 ;
-use IO::Uncompress::Adapter::LZO  2.039 ;
+use IO::Uncompress::Base  2.040 ;
+use IO::Uncompress::Adapter::LZO  2.040 ;
 use Compress::LZO qw(crc32 adler32);
-use IO::Compress::Lzop::Constants  2.039 ;
+use IO::Compress::Lzop::Constants  2.040 ;
 
 
 require Exporter ;
 our ($VERSION, @ISA, @EXPORT_OK, %EXPORT_TAGS, $UnLzopError);
 
-$VERSION = '2.039';
+$VERSION = '2.040';
 $UnLzopError = '';
 
 @ISA    = qw( Exporter IO::Uncompress::Base );

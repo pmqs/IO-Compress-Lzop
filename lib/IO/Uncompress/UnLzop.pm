8e1f7434e7b863732a874617f0726b878ee91011
pmqs <pmqs@cpan.org>
2019-12-04 22:03:50 +0000

    2.092

--- lib/IO/Uncompress/UnLzop.pm
+++ lib/IO/Uncompress/UnLzop.pm
@@ -4,18 +4,18 @@ use strict ;
 use warnings;
 use bytes;
 
-use IO::Compress::Base::Common  2.091 qw(:Status createSelfTiedObject);
+use IO::Compress::Base::Common  2.092 qw(:Status createSelfTiedObject);
 
-use IO::Uncompress::Base  2.091 ;
-use IO::Uncompress::Adapter::LZO  2.091 ;
+use IO::Uncompress::Base  2.092 ;
+use IO::Uncompress::Adapter::LZO  2.092 ;
 use Compress::LZO qw(crc32 adler32);
-use IO::Compress::Lzop::Constants  2.091 ;
+use IO::Compress::Lzop::Constants  2.092 ;
 
 
 require Exporter ;
 our ($VERSION, @ISA, @EXPORT_OK, %EXPORT_TAGS, $UnLzopError);
 
-$VERSION = '2.091';
+$VERSION = '2.092';
 $UnLzopError = '';
 
 @ISA    = qw( IO::Uncompress::Base Exporter );
